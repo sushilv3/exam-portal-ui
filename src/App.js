6d4c88cdf2932f43828ce07107d786bb1065750e
sushilv3 <sushilv3rma@gmail.com>
2021-05-26 17:26:25 +0530

    added switch and route components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import React from 'react';
+import {Route,Switch} from 'react-router-dom';
 import './App.css';
-
+import RegistrationForm from './components/pages/RegistrationForm';
+import Error from './components/pages/Error';
 function App() {
   return (
-    <div >
-     <h1>App component</h1>
+    <div className="container">
+    <Switch>
+     <Route exact path="/" component={RegistrationForm} />
+     <Route component={Error} />
+     </Switch>
     </div>
   );
 }
