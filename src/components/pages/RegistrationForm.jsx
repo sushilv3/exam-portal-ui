d52e4bd29553c17b63eed2782d4764afcc3a9bc8
sushilv3 <sushilv3rma@gmail.com>
2021-05-27 07:35:48 +0530

    registration form submitted

--- src/components/pages/RegistrationForm.jsx
+++ src/components/pages/RegistrationForm.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../NavBar";
 import { makeStyles } from "@material-ui/core/styles";
-import { TextField,Button } from "@material-ui/core";
+import { TextField, Button } from "@material-ui/core";
+import axios from "axios";
+import base_url from '../../api/bootApi';
 
 const RegistrationForm = () => {
+  const [userData, setUserData] = useState({
+    userName: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+
+  const inputEvent = (event) => {
+    // console.log(event.target.value);
+    console.log(event.target.userName);
+    console.log(event.target.password);
+    console.log(event.target.firstName);
+    console.log(event.target.lastName);
+    console.log(event.target.email);
+    console.log(event.target.phone);
+
+    const { value, name } = event.target;
+    setUserData((preValue) => {
+      console.log("@@@@@@@@@@@@@@@@@ ", preValue);
+      return {
+        ...preValue,
+        [name]: value,
+      };
+    });
+  };
+
+  const RegistrationFormSubmit = (e) => {
+    console.log("form submitted");
+    console.log("called inside from resgistration form submittion", userData);
+    postDataToServer(userData);
+    e.preventDefault();
+  };
+
+  const postDataToServer = (userData) => {
+    axios.post(`${base_url}/user/`,userData).then((response) => {
+      console.log(response);
+      console.log("success");
+    },(error)=>{
+      console.log(error);
+      console.log("something went wrong");
+    });
+  };
+
   return (
     <div className="container">
       <div className="row mt-3">
@@ -13,36 +60,51 @@ const RegistrationForm = () => {
           </div>
           <h1 className="text-center">Register Here!!!</h1>
 
-          <form>
+          <form onSubmit={RegistrationFormSubmit}>
             {/**userName field*/}
             <TextField
               className="form-control mb-3"
+              name="userName"
+              onChange={inputEvent}
+              value={userData.userName}
               id="standard-basic"
               label="User Name"
             />
 
-            {/**userName field*/}
+            {/**password field*/}
             <TextField
               className="form-control mb-3"
+              name="password"
+              onChange={inputEvent}
+              value={userData.password}
               id="standard-basic"
               label="Password"
             />
             {/**firstName field*/}
             <TextField
               className="form-control mb-3"
+              name="firstName"
+              onChange={inputEvent}
+              value={userData.firstName}
               id="standard-basic"
               label="First Name"
             />
             {/**lastName field*/}
             <TextField
               className="form-control mb-3"
+              name="lastName"
+              onChange={inputEvent}
+              value={userData.lastName}
               id="standard-basic"
               label="Last Name"
             />
             {/**e-mail field*/}
             <TextField
-              type="eamil"
+              type="email"
               className="form-control mb-3"
+              name="email"
+              onChange={inputEvent}
+              value={userData.email}
               id="standard-basic"
               label="E-mail Address"
             />
@@ -50,6 +112,9 @@ const RegistrationForm = () => {
             <TextField
               type="number"
               className="form-control mb-3"
+              name="phone"
+              onChange={inputEvent}
+              value={userData.phone}
               id="standard-basic"
               label="Phone Number"
             />
@@ -60,7 +125,6 @@ const RegistrationForm = () => {
               <Button className="ms-3" variant="contained" color="secondary">
                 Clear
               </Button>
-
             </div>
           </form>
         </div>
