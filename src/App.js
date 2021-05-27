17399137ff27cae5fc1f92199fea3e9cc5409c10
sushilv3 <sushilv3rma@gmail.com>
2021-05-27 10:52:33 +0530

    import navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import {Route,Switch} from 'react-router-dom';
-import './App.css';
-import RegistrationForm from './components/pages/RegistrationForm';
-import Error from './components/pages/Error';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import RegistrationForm from "./components/pages/RegistrationForm";
+import Error from "./components/pages/Error";
+import Login from "./components/pages/Login";
+import Home from "./components/pages/Home";
+import NavBar from "./components/NavBar";
+import AboutUs from "./components/pages/AboutUs";
+import Service from "./components/pages/Service";
+import Contact from "./components/pages/Contact";
 function App() {
   return (
     <div className="container">
-    <Switch>
-     <Route exact path="/" component={RegistrationForm} />
-     <Route component={Error} />
-     </Switch>
+    <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={RegistrationForm} />
+        <Route exact path="/signin" component={Login} />
+        <Route exact path="/about" component={AboutUs} />
+        <Route exact path="/service" component={Service} />
+        <Route exact path="/contact" component={Contact}  />
+        
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
